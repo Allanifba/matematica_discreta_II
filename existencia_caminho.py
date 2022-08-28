cd0c589e986ffa23e12c23985dbf5112780ba669
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-08-28 18:27:26 -0300

    Update existencia_caminho.py

--- existencia_caminho.py
+++ existencia_caminho.py
@@ -1,5 +1,4 @@
 # programa que verifica se existe um caminho simples entre dois vértices distintos de um grafo G
-# of a graph
 
 from collections import defaultdict
 import numpy as np
@@ -10,49 +9,49 @@ def restart_program():
     os.execl(python, python, * sys.argv)
 
 while True:
-    # This class represents a directed graph using adjacency list representation
+    # Esta classe representa um gráfico direcionado usando a representação por meio de uma lista de adjacências
     class Graph:
 
         def __init__(self, vertices):
-            self.V = vertices  # No. of vertices
-            self.graph = defaultdict(list)  # default dictionary to store graph
+            self.V = vertices  # Número de vértices
+            self.graph = defaultdict(list)  # Dicionário padrão para armazenar o grafo
 
-        # function to add an edge to graph
+        # Função para adicionar um vértice ao grafo.
         def addEdge(self, u, v):
             self.graph[u].append(v)
 
-        # Use BFS to check path between s and d
+        # Usando a busca em largura (BFS) para checar a existência de uma caminho entre s e d
         def isReachable(self, s, d):
-            # Mark all the vertices as not visited
+            # Marca todos os vértices como não visitados
             visited = [False] * (self.V)
 
-            # Create a queue for BFS
+            # Criar uma fila para a busca em largura
             queue = []
 
-            # Mark the source node as visited and enqueue it
+            # Marcar o nó de origem como visitado e o enfileira
+
             queue.append(s)
             visited[s] = True
 
             while queue:
 
-                # Dequeue a vertex from queue
+                # Desenfileira um vértice da fila
                 n = queue.pop(0)
 
-                # If this adjacent node is the destination node,
-                # then return true
+                # Se este nó adjacente for o nó de destino, então retorne true
                 if n == d:
                     return True
 
-                # Else, continue to do BFS
+                # Caso contrário, continue a fazer busca em largura
                 for i in self.graph[n]:
                     if visited[i] == False:
                         queue.append(i)
                         visited[i] = True
-            # If BFS is complete without visited d
+            # Se a busca em largura estiver completo sem visitar d
             return False
 
 
-    # Create a graph given in the above diagram
+    # Instruções ao usuário e contribuições pessoais
 
 
     print('\n-----------------------------------  DESCRIÇÃO  ------------------------------------')
@@ -68,20 +67,29 @@ while True:
               '                                       2------->3                                 \n'
               'terá a seguinte matriz de adjacência: [[0,1,1,0],[0,0,0,0],[0,1,0,1],[0,0,0,0]]')
     print('-----------------------------  ENTRE COM OS DADOS  ---------------------------------')
-    A = input('Digite a matriz de adjacência: ')
+    
+    # Dados a serem inseridos pelo usuário
+    A = input('Digite a matriz de adjacência: ') 
     u = int(input('Digite o vértice inicial: '))
     v = int(input('Digite o vértice final: '))
+    
+    # Matriz de adjacência interna gerada
     arr = np.array(eval(A))
     g = Graph(len(arr))
+    
+    
     if u==v:
         print('Os vértices devem ser distintos.')
+        
+    # Criação da lista de adjacência a partir da matriz de adjacência dada pelo usuário
     if u!=v:
         print('\n ***********************************  RESPOSTA  ***********************************')
         for i in range(0,len(arr)):
             for j in range(0,len(arr)):
                 if arr[i,j]!=0:
                     g.addEdge(i,j)
-
+        
+        #Saídas para o usuário
         if g.isReachable(u, v):
             print(" Existe um caminho do vértice %d para o vértice %d" % (u, v))
         else:
